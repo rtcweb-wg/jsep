274e2e160af10c5256fc4cee3a93c868dceb5e74
EKR <ekr@rtfm.com>
2014-05-22 15:50:13 -0700

    Update travis

---
+++ upload-draft.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+import shutil
+
+if len(sys.argv) != 2:
+    sys.exit(1)
+
+built = sys.argv[1]
+
+print os.environ['GH_TOKEN']
+
+sys.exit(0)
+
+os.mkdir("out")
+os.chdir("out")
+subprocess.check_call(["git","init","."])
+shutil.move("upload-tmp", built)
+subprocess.check_call(["git","add",built])
+subprocess.check_call(["git","commit","-m", "Commit"])
+subprocess.check_call(["git","push","origin","gh-pages"])
+
+
